448aefc198a37fc4dad6ffd88a8fbcc794514a67
TheIncredibleJerbear <78756889+TheIncredibleJerbear@users.noreply.github.com>
2021-03-07 12:32:09 -0500

    Add files via upload

---
+++ homework/homework/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  homework
+//
+//  Created by Jerry Eapen on 3/2/21.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var BillAmountlabel: UILabel!
+    @IBOutlet weak var BillAmounttextfeild: UITextField!
+    @IBOutlet var TipAmountlabel: UIView!
+    @IBOutlet weak var tenpercentlabel: UILabel!
+    @IBOutlet weak var fifteenpercentlabel: UILabel!
+    @IBOutlet weak var twentypercentlabel: UILabel!
+    @IBAction func calculatebutton(_ sender: Any)
+   {
+        //WILLIAM FIGUERO'S CODE REFERENCE
+        if let finalamount = BillAmounttextfeild.text
+        {
+        let billtotal = Double(finalamount) ?? 0
+            print(billtotal)
+        //WILLIAM FIGUERO'S REFERENCE
+            
+            //CALCULATION
+            let tenpercentcalculation = String(format: "%.2f",billtotal * 0.10)
+            let fifteenpercentcalculation = String(format: "%.2f",billtotal * 0.15)
+            let twentypercentcalculation = String(format: "%.2f", billtotal * 0.20)
+            
+            //DISPLAY
+            tenpercentlabel.text = "Your 10 Percent tip is $ \(tenpercentcalculation)"
+            fifteenpercentlabel.text = "Your 15 Percent tip is $ \(fifteenpercentcalculation)"
+            twentypercentlabel.text = "Your 20 Percent tip is $\(twentypercentcalculation)"
+        
+        }
+        
+    }
+
+}
